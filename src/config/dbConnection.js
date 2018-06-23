ab34c21daf140f22324c4b6b8c9a189cd5fd5563
Rodrigo Cordova Whiteley <r.a.c.whiteley@gmail.com>
2018-06-23 05:07:19 -0400

    - Base de datos actualizada - Cambios a la base de datos agregados al proyecto.

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,15 @@ const mysql = require('mysql');
 
 module.exports = () => {
     return mysql.createConnection({
+        /*host: 'localhost',
+        user: 'root',
+        password: '3090323',
+        database: 'futbol_db'*/
+
         host: 'db4free.net',
-        user: 'pinera',
-        password: 'pineraql',
-        database: 'futbol_udec'
+        user: 'pineraql1',
+        password: 'pineraql1',
+        database: 'futbol_db'
+
     });
 };
--- src/database/mysqldal.js
+++ src/database/mysqldal.js
@@ -19,20 +19,15 @@ exports.getStandings = function (tournyId, callback) {
 
 exports.getGames = function (tournyId, callback) {
     connection.query(
-        "SELECT partidos.id, partidos.equipoLocal_nombre, partidos.equipoVisita_nombre, partidos.fecha, partidos.marcadorLocal, \
-            partidos.marcadorVisita \
-            FROM partidos INNER JOIN torneos \
-            ON torneos.id=partidos.torneos_id \
-            WHERE torneos.id = ?;", [tournyId], callback);
+        "SELECT * FROM partidos_view where torneos_id=?", [tournyId], callback);
 };
 
 exports.getTeamsInTourny = function (tournyId, callback) {
     connection.query(
-        "select torneo_equipo.equipos_nombre\
-            from torneo_equipo\
-            INNER JOIN torneos\
-            ON torneos.id=torneo_equipo.torneos_id\
-            WHERE torneos.id=?;", [tournyId], callback);
+        "select " +
+        "equipos_torneos.equipos_nombre" +
+        "from equipos_torneos" +
+        "where equipos_torneos.torneos_id = ?;", [tournyId], callback);
 };
 
 exports.getTeams = function (callback) {
@@ -41,9 +36,7 @@ exports.getTeams = function (callback) {
 
 exports.getGoalScorers = function (tournyId, callback) {
     connection.query(
-        "select jugadores_matricula, nombre, carrera, equipos_nombre,\
-        goles from jugadores_torneo_view where jugadores_torneo_view.torneos_id = ?\
-        and goles > 0 order by goles desc;", [tournyId], callback);
+        "select * from goles_view where torneos_id = ? and goles > 0 order by goles desc;", [tournyId], callback);
 };
 
 exports.addTeam = function (nombre, facultad, callback) {
@@ -52,15 +45,15 @@ exports.addTeam = function (nombre, facultad, callback) {
 };
 
 exports.getTeamPlayersInTourny = function (tournyId, equipo, callback) {
-    connection.query('select * from jugadores_torneo_view where torneos_id=? and equipos_nombre=?;', [tournyId, equipo], callback);
+    connection.query('select * from plantel_detalles_view where torneos_id=? and equipos_nombre=?;', [tournyId, equipo], callback);
 };
 
 exports.getGameInfo = function (partido, callback) {
-    connection.query("select * from( \
-select partidos_id, nombre, matricula, equipos_nombre, torneos_id, minuto, 'gol' as evento \
-from goles_view as T1 \
-union select \
-partidos_id, jugadores_nombre, matricula, equipos_nombre, torneos_id, minuto, IF(tarjetas_id = 1, 'amarilla', 'roja') as evento \
-from sanciones_view as T2) as T3 \
-where partidos_id=? order by minuto;", [partido], callback);
+    connection.query("select * from( " +
+        "select partidos_id, nombre, jugadores_matricula, equipos_nombre, minuto, 'gol' as evento " +
+        "from goles_jugador_view as T1 " +
+        "union select " +
+        "partidos_id, nombre, jugadores_matricula, equipos_nombre, minuto, IF(tarjetas_id = 1, 'amarilla', 'roja') as evento " +
+        "from sanciones_jugador_view as T2) as T3 " +
+        "where partidos_id=? order by minuto;", [partido], callback);
 };
